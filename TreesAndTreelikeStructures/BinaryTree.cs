82e95d893a259d4f8f44de72b74924e5d11795ee
Kamen <kamenminkov.com@abv.bg>
2017-06-08 23:36:37 +0300

    tree modified, aditional methods

---
+++ TreesAndTreelikeStructures/BinaryTree.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+public class BinaryTree<T> where T : IComparable
+{
+    private Node rootNode;
+    private int Count;
+
+    private class Node
+    {
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
+        public T Value { get; set; }
+
+        public Node(T value)
+        {
+            this.Value = value;
+        }
+    }
+
+    public T Value { get; set; }
+    public BinaryTree<T> LeftChild { get; set; }
+    public BinaryTree<T> RightChild { get; set; }
+
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        this.Value = value;
+        this.RightChild = rightChild;
+        this.LeftChild = leftChild;
+    }
+
+    public void Insert(T value)
+    {
+        Node newNode = new Node(value);
+        if (Count == 0)
+        {
+            this.rootNode = newNode;
+            return;
+        }
+
+        Node currentNode = rootNode;
+        Node parent = rootNode;
+        while (true)
+        {
+            if (currentNode.Value.CompareTo(newNode.Value) < 0)
+            {
+                parent = currentNode;
+                currentNode = currentNode.LeftChild;
+            } else if (currentNode.Value.CompareTo(newNode.Value) > 0)
+            {
+                parent = currentNode;
+                currentNode = currentNode.RightChild;
+            }
+            if (currentNode == null)
+            {
+                currentNode = newNode;
+                return;
+            }
+        }
+    }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', 2*indent) + this.Value);
+
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.PrintIndentedPreOrder(indent+1);
+        }
+        if (this.RightChild != null)
+        {
+            this.RightChild.PrintIndentedPreOrder(indent+1);
+        }
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.EachInOrder(action);
+        }
+
+        action(this.Value);
+
+        if (this.RightChild != null)
+        {
+            this.RightChild.EachInOrder(action);
+        }
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.EachPostOrder(action);
+        }
+
+        if (this.RightChild != null)
+        {
+            this.RightChild.EachPostOrder(action);
+        }
+
+        action(this.Value);
+    }
+}
--- TreesAndTreelikeStructures/Tree.cs
+++ TreesAndTreelikeStructures/Tree.cs
@@ -5,6 +5,8 @@ public class Tree<T>
 {
     public T Value { get; set; }
     public IList<Tree<T>> Children { get; set; }
+    public Tree<T> Parent { get; set; }
+
 
     public Tree(T value, params Tree<T>[] children)
     {
@@ -18,13 +20,12 @@ public class Tree<T>
 
     public void Print(int indent = 0)
     {
-        Console.Write(string.Join(" ", 2 * indent));
-
+        Console.Write(new string(' ', 2 * indent));
+        Console.WriteLine(this.Value);
         foreach (var child in this.Children)
         {
-            child.Print(indent+1);
+            child.Print(indent + 1);
         }
-        Console.WriteLine(this.Value);
     }
 
     public void Each(Action<T> action)
@@ -52,7 +53,7 @@ public class Tree<T>
 
         while (queue.Count > 0)
         {
-            var current =  queue.Dequeue();
+            var current = queue.Dequeue();
             foreach (var child in current.Children)
             {
                 queue.Enqueue(child);
@@ -70,5 +71,46 @@ public class Tree<T>
         }
 
         result.Add(tree.Value);
+    }
+
+    public List<Tree<T>> GetMaxDepth()
+    {
+        List<Tree<T>> path = new List<Tree<T>>();
+
+        foreach (var child in this.Children)
+        {
+            var help = child.GetMaxDepth();
+            if (help.Count > path.Count)
+            {
+                path = help;
+            }
+        }
+
+        path.Add(this);
+        return path;
+    }
+
+    public static void GetAllPaths(Tree<T> node, List<Tree<T>> path, List<List<Tree<T>>> paths)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        path.Add(node);
+
+        if (node.Children.Count == 0)
+        {
+            List<Tree<T>> newPath = new List<Tree<T>>(path);
+            paths.Add(newPath);
+
+        }
+
+        foreach (var child in node.Children)
+        {
+            GetAllPaths(child, path, paths);
+        }
+
+        path.RemoveAt(path.Count - 1);
     }
 }
